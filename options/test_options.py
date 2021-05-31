e4c53f7e089353d1e2cbfcc95a332b7a9d2d1b5c
Alexandru Leorda <leroda@realisti.co>
2021-05-31 14:14:20 +0200

    FIXME

--- options/test_options.py
+++ options/test_options.py
@@ -10,11 +10,6 @@ class TestOptions(BaseOptions):
         parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.set_defaults(model='test')
-
-        parser.add_argument('--test_path', type=str, default='./TestData/TestWhole', help='test images path')
-        parser.add_argument('--results_dir', type=str, default='./Results/TestWholeResults', help='saves results here.')
-        parser.add_argument('--upscale_factor', type=int, default=4, help='upscale factor for the whole input image (not for face)')
-
         
         self.isTrain = False
         return parser
